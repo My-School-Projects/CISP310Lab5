648fa010f2c8fd15c28c10b9ad00d458b978c65d
Michael Dorst <code@mikefdorst.com>
2016-09-29 02:28:12 -0700

    Do the math

--- windows32/main.asm
+++ windows32/main.asm
@@ -12,6 +12,8 @@ INCLUDE io.h   ; header file for input/output
 
 	; We will be using DWORDS, because scores of more than 65535 (2^16 - 1) seem reasonable,
 	; and scores of more than 4294967295 (2^32 - 1) do not.
+	; We will treat all values as unsigned integers, because this problem does not involve
+	; any negative numbers.
 	
 	grade1			DWORD	0
 	grade2			DWORD	0
@@ -67,63 +69,96 @@ _MainProc PROC
 	
 	; Get grade1 and weight1
 
-	mov		gradeNumber,	"1"			; Set gradePrompt to "Grade 1:"
-	mov		weightNumber,	"1"			; Set weightPrompt to "Weight 1:"
+	mov		gradeNumber,		"1"			; Set gradePrompt to "Grade 1:"
+	mov		weightNumber,		"1"			; Set weightPrompt to "Weight 1:"
 
-	input	gradePrompt,	string,	40	; Display prompt, string := user input (as string)
-	atod	string						; EAX := user input (as integer)
-	mov		grade1,			eax			; grade1 := EAX
+	input	gradePrompt,		string,	40	; Display prompt, string := user input (as string)
+	atod	string							; EAX := user input (as integer)
+	mov		grade1,				eax			; grade1 := EAX
 
-	input	weightPrompt,	string, 40
+	input	weightPrompt,		string, 40
 	atod	string
-	mov		weight1,		eax			; weight1 := user input (as integer)
+	mov		weight1,			eax			; weight1 := user input (as integer)
 
 	; Repeat the same steps as above for grades 2, 3 and 4
 
 	; Get grade2 and weight2
 
-	mov		gradeNumber,	"2"			; Set prompt to "Grade 2:"
-	mov		weightNumber,	"2"			; Set weightPrompt to "Weight 2:"
+	mov		gradeNumber,		"2"			; Set prompt to "Grade 2:"
+	mov		weightNumber,		"2"			; Set weightPrompt to "Weight 2:"
 										
-	input	gradePrompt,	string,	40
+	input	gradePrompt,		string,	40
 	atod	string
-	mov		grade2,			eax
+	mov		grade2,				eax
 
-	input	weightPrompt,	string, 40
+	input	weightPrompt,		string, 40
 	atod	string
-	mov		weight2,		eax
+	mov		weight2,			eax
 
 	; Get grade3 and weight3
 
-	mov		gradeNumber,	"3"
-	mov		weightNumber,	"3"
+	mov		gradeNumber,		"3"
+	mov		weightNumber,		"3"
 
-	input	gradePrompt,	string,	40
+	input	gradePrompt,		string,	40
 	atod	string
-	mov		grade3,			eax
+	mov		grade3,				eax
 
-	input	weightPrompt,	string, 40
+	input	weightPrompt,		string, 40
 	atod	string
-	mov		weight3,		eax
+	mov		weight3,			eax
 	
 	; Get grade4 and weight4
 
-	mov		gradeNumber,	"4"
-	mov		weightNumber,	"4"
+	mov		gradeNumber,		"4"
+	mov		weightNumber,		"4"
 
-	input	gradePrompt,	string,	40
+	input	gradePrompt,		string,	40
 	atod	string
-	mov		grade4,			eax
+	mov		grade4,				eax
 
-	input	weightPrompt,	string,	40
+	input	weightPrompt,		string,	40
 	atod	string
 	mov		weight4,			eax
 
 	; Do the math!
 
-	; TODO
+	mov		eax,				grade1
+	mul		weight1
+	mov		weightedGrade1,		eax				; weightedGrade1 := grade1 * weight1
 
-	mov		eax,			0			; exit with return code 0
+	mov		eax,				grade2
+	mul		weight2
+	mov		weightedGrade2,	 eax				; weightedGrade2 := grade2 * weight2
+
+	mov		eax,				grade3
+	mul		weight3
+	mov		weightedGrade3,		eax				; weightedGrade3 := grade3 * weight3
+
+	mov		eax,				grade4
+	mul		weight4
+	mov		weightedGrade4,		eax				; weightedGrade4 := grade4 * weight4
+
+	mov		eax,				weightedGrade1	; EAX := weightedGrade1
+	add		eax,				weightedGrade2	; EAX := weightedGrade1 + weightedGrade2
+	add		eax,				weightedGrade3	; EAX := weightedGrade1 + weightedGrade2 + weightedGrade3
+	add		eax,				weightedGrade4	; EAX := weightedGrade1 + weightedGrade2 + weightedGrade3 + weightedGrade4
+	mov		weightedSum,		eax				; weightedSum := EAX
+
+	mov		eax,				weight1			; EAX := weight1
+	add		eax,				weight2			; EAX := weight1 + weight2
+	add		eax,				weight3			; EAX := weight1 + weight2 + weight3
+	add		eax,				weight4			; EAX := weight1 + weight2 + weight3 + weight4
+	mov		sumOfWeights,		eax				; sumOfWeights := EAX
+
+	; Division by a DWORD requires the dividend to be stored in EDX:EAX
+	mov		edx,				0				; EDX := 0 (our dividend is only 32 bits - only the lower order bits are used)
+	mov		eax,				weightedSum		; EAX := weightedSum
+	div		sumOfWeights						; EAX := weightedSum / sumOfWeights
+	mov		weightedAverage,	eax				; weightedAverage := EAX
+
+	mov		eax,				0				; exit with return code 0
+	
 	ret
 _MainProc ENDP
 
